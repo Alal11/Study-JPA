cea74762083430814db184d8561f26022e3792d5
Alal11 <spalqj56@naver.com>
2024-11-14 21:36:28 +0900

    객체 지향 모델링x, 객체를 테이블에 맞춰 데이터 중심으로 모델링한 경우

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,37 +1,41 @@
-//package hellojpa;
-//
-//import jakarta.persistence.*;
-//
-//@Entity
-//@SequenceGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        sequenceName = "MEMBER_SEQ",  // 매핑할 데이터베이스 시퀀스 이름
-//        initialValue = 1, allocationSize = 50)
-//public class Member {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-//            generator = "MEMBER_SEQ_GENERATOR")
-//    private Long id;
-//    @Column(name = "name", nullable = false)
-//    private String username;
-//
-//    public Member() {
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//}
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "TEAM_ID")
+    private Long teamId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+}
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,62 +1,62 @@
-package jpabook.jpashop.domain;
-
-import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "ORDERS")
-public class Order {
-
-    @Id
-    @GeneratedValue
-    @Column(name = "ORDER_ID")
-    private Long id;
-    @Column(name = "MEMBER_ID")
-    private Long memberId;  // 테이블 설계에 맞춰 외래키 가져오는 방식
-
-    private Member member;  // 참조 값을 그대로 가져와야 객체지향적 설계 가능
-
-    public Member getMember() {
-        return member;
-    }
-    public void setMember(Member member) {
-        this.member = member;
-    }
-
-    private LocalDateTime orderDate;
-    @Enumerated(EnumType.STRING)
-    private OrderStatus status;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
-}
+//package jpabook.jpashop.domain;
+//
+//import jakarta.persistence.*;
+//
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Table(name = "ORDERS")
+//public class Order {
+//
+//    @Id
+//    @GeneratedValue
+//    @Column(name = "ORDER_ID")
+//    private Long id;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;  // 테이블 설계에 맞춰 외래키 가져오는 방식
+//
+//    private Member member;  // 참조 값을 그대로 가져와야 객체지향적 설계 가능
+//
+//    public Member getMember() {
+//        return member;
+//    }
+//    public void setMember(Member member) {
+//        this.member = member;
+//    }
+//
+//    private LocalDateTime orderDate;
+//    @Enumerated(EnumType.STRING)
+//    private OrderStatus status;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getMemberId() {
+//        return memberId;
+//    }
+//
+//    public void setMemberId(Long memberId) {
+//        this.memberId = memberId;
+//    }
+//
+//    public LocalDateTime getOrderDate() {
+//        return orderDate;
+//    }
+//
+//    public void setOrderDate(LocalDateTime orderDate) {
+//        this.orderDate = orderDate;
+//    }
+//
+//    public OrderStatus getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(OrderStatus status) {
+//        this.status = status;
+//    }
+//}
