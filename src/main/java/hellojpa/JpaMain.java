fc82b85bb98c026b068791c468848b9e968f2c3e
Alal11 <spalqj56@naver.com>
2024-11-15 13:56:41 +0900

    객체 지향 모델링o, 양방향 연관관계와 연관관계의 주인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -20,17 +22,21 @@ public class JpaMain {
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeam(team);
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
+            team.addMember(member);
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam.name = " + findTeam.getName());
+            em.flush();
+            em.clear();
 
-            // 수정
-            Team newTeam = em.find(Team.class, 100L);
-            findMember.setTeam(newTeam);
+            Team findTeam = em.find(Team.class, team.getId());  // 1차 캐시
+            List<Member> members = findTeam.getMembers();
+
+            System.out.println("================");
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
+            System.out.println("================");
 
             tx.commit();
         } catch (Exception e) {
