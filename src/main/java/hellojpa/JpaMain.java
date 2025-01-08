9d67003295b7b9482532db078d7107cc75eb9141
Alal11 <spalqj56@naver.com>
2025-01-08 17:28:19 +0900

    실습/예제 코드 주석 체인지

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,38 +1,38 @@
-//package hellojpa;
-//
-//import jakarta.persistence.*;
-//
-//import java.time.LocalDateTime;
-//
-//public class JpaMain {
-//
-//    public static void main(String[] args) {
-//
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//        EntityManager em = emf.createEntityManager();
-//
-//        EntityTransaction tx = em.getTransaction();
-//        tx.begin();
-//
-//        try {
-//            Member member = new Member();
-//            member.setUsername("user1");
-//            member.setCreatedBy("Kim");
-//            member.setCreateDate(LocalDateTime.now());
-//
-//            em.persist(member);
-//
-//            em.flush();
-//            em.clear();
-//
-//            tx.commit();
-//        } catch (Exception e) {
-//            tx.rollback();
-//        } finally {
-//            em.close();
-//        }
-//
-//        emf.close();
-//    }
-//}
-//
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("Kim");
+            member.setCreateDate(LocalDateTime.now());
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
+
